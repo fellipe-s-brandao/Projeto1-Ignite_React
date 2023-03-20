750edb756954d25a0fd7d39e43ea0598f17bbb66
Fellipe_Brandao <fellipesilverio31@hotmail.com>
2023-03-20 13:32:30 -0300

    criando componente post

---
+++ src/components/Post.jsx
@@ -0,0 +1,28 @@
+import styles from "./Post.module.css"
+
+export function Post() {
+    return (
+        <article className={styles.Post}>
+            <header>
+                <div className={styles.author}>
+                    <img className={styles.avatar} src="https://github.com/fellipe-s-brandao.png" />
+
+                    <div className={styles.authorInfo}>
+                        <strong>Fellipe Brandao</strong>
+                        <span>Desenvolvedor</span>
+                    </div>
+                </div>
+
+                <time title="11 de Maio de 2022 às 08:12" dateTime="2022-05-11 08:12:30">Publicado ha 1 hora</time>
+            </header>
+
+            <div className={styles.content}>
+                <p>Fala galeraa 👋</p>
+                <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
+                <p>👉 <a href="">jane.design/doctorcare</a> </p>
+                <p> <a href="">#novoprojeto #nlw #rocketseat</a> </p>
+            </div>
+
+        </article>
+    )
+}
