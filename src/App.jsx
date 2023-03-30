b537e3944689f6eb2591debb3c2ebd4fe96a37f6
Fellipe_Brandao <fellipesilverio31@hotmail.com>
2023-03-30 13:15:08 -0300

    feat: iterando os posts

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,51 @@ import { Post } from './components/Post'
 import styles from './App.module.css'
 import './global.css'
 
+const posts = [
+    {
+        id: 1,
+        author: {
+            avatarUrl: "https://github.com/fellipe-s-brandao.png",
+            name: "Fellipe Brandao",
+            role: "Dev backend"
+        },
+        content: [
+            { type: 'paragraph', content:'Fala galeraa 👋' },
+            { type: 'paragraph', content:'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
+            { type: 'link', content:'jane.design/doctorcare' }
+        ],
+        publishAt: new Date('2023-03-29 14:00:00')
+    },
+    {
+        id: 2,
+        author: {
+            avatarUrl: "https://github.com/diego3g.png",
+            name: "Diego Fernandes",
+            role: "CTO Rocketseat"
+        },
+        content: [
+            { type: 'paragraph', content:'Fala galeraa 👋' },
+            { type: 'paragraph', content:'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
+            { type: 'link', content:'jane.design/doctorcare' }
+        ],
+        publishAt: new Date('2023-03-28 14:00:00')
+    },
+    {
+        id: 3,
+        author: {
+            avatarUrl: "https://github.com/maykebrito.png",
+            name: "Mayke Brito",
+            role: "Educador"
+        },
+        content: [
+            { type: 'paragraph', content:'Fala galeraa 👋' },
+            { type: 'paragraph', content:'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
+            { type: 'link', content:'jane.design/doctorcare' }
+        ],
+        publishAt: new Date('2023-03-27 14:00:00')
+    }
+]
+
 function App() {
     return (
         <div >
@@ -14,10 +59,15 @@ function App() {
                 <Sidebar />
 
                 <main>
-                    <Post>
-                    </Post>
-                    <Post>
-                    </Post>
+                    {
+                        posts.map(post => (
+                            <Post
+                                author={post.author}
+                                content={post.content}
+                                publishAt={post.publishAt}
+                            />
+                        ))
+                    }
                 </main>
             </div>
         </div>
