700a2c17c5a30287e70196c473054de982e769ba
Fellipe_Brandao <fellipesilverio31@hotmail.com>
2023-04-02 17:48:30 -0300

    feat: ajustando curtida de comentario

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,10 +1,17 @@
 import { ThumbsUp, Trash } from "phosphor-react";
 import { Avatar } from "./Avatar";
 import styles from "./Comment.module.css";
+import { useState } from "react";
 
 export function Comment({ content, OnDeleteComment }) {
+    const [likeCount, setLikeCount] = useState(0);
+
     function handleDeleteComment() {
         OnDeleteComment(content)
+    }
+
+    function handleLikeComment() {
+        setLikeCount(likeCount + 1);
     }
 
     return (
@@ -29,9 +36,9 @@ export function Comment({ content, OnDeleteComment }) {
                 </div>
 
                 <footer>
-                    <button>
+                    <button onClick={handleLikeComment}>
                         <ThumbsUp/>
-                        Aplaudir <span>20</span>
+                        Aplaudir <span>{likeCount}</span>
                     </button>
                 </footer>
             </div>
